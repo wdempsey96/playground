9a1bb8577f010592c139ea70df19f14743ecd454
Will Dempsey <wdempsee@umich.edu>
2019-01-17 19:43:29 -0500

    ADD: started mergesort implementation

---
+++ mergesort.py
@@ -0,0 +1,10 @@
+"""Mergesort implementation."""
+
+
+def mergesort(arr, left, right):
+    pass
+
+
+UNSORTED_ARRAY = [6, 4, 1, 7, 2, 3, 0, 5, 81, 18, 999, 205, 63, 24, 22, 91, 82]
+mergesort(UNSORTED_ARRAY, 0, len(UNSORTED_ARRAY) - 1)
+print("Sorted array: {}".format(UNSORTED_ARRAY))
